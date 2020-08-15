df126d7cd7a4f40760d59eb1718a8b1bcb746abc
satyasaim <satyasaimedia3@gmail.com>
2020-08-15 13:09:18 +0530

    flutter course

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(FirstApp());
+}
+
+class FirstApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(home: Text('Hello sai'));
+  }
+}
